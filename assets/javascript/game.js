a46dcb8beac1d575df203a7e84b5f257080eed9c
John Pendergrass <john.pendergrass@me.com>
2020-03-01 20:19:29 -0600

    added crystal backgrounds with padding, added clicks for each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,21 +15,26 @@ $(document).ready(function(){
     $("#losses-num").append(losses).css('text-align', 'center');
     $("#score-counter").append(counter).css('text-align', 'center');
 
-    // variable with 4 crystals as objects which will be random numbers from 1 - 12
+    // variable with 4 crystals as objects with values of 0
     var crystals = {
         crystal1: {
-            points: Math.floor(Math.random() * 12 + 1),
+            value: 0,
         },
         crystal2: {
-            points: Math.floor(Math.random() * 12 + 1),
+            value: 0,
         },
         crystal3: {
-            points: Math.floor(Math.random() * 12 + 1),
+            value: 0,
         },
         crystal4: {
-            points: Math.floor(Math.random() * 12 + 1),
+            value: 0,
         }
-    }
+    };
+    // rewriting the 0 value with random numbers
+    crystals.crystal1.value = Math.floor(Math.random() * 12 + 1);
+    crystals.crystal2.value = Math.floor(Math.random() * 12 + 1);
+    crystals.crystal3.value = Math.floor(Math.random() * 12 + 1);
+    crystals.crystal4.value = Math.floor(Math.random() * 12 + 1); 
 
 // Reset game
 function reset() {
@@ -37,27 +42,60 @@ function reset() {
     $("#number-to-guess").text(targetRandomNum).css('text-align', 'center');
     var crystals = {
         crystal1: {
-            points: Math.floor(Math.random() * 12 + 1),
+            value: 0,
         },
         crystal2: {
-            points: Math.floor(Math.random() * 12 + 1),
+            value: 0,
         },
         crystal3: {
-            points: Math.floor(Math.random() * 12 + 1),
+            value: 0,
         },
         crystal4: {
-            points: Math.floor(Math.random() * 12 + 1),
+            value: 0,
         }
     }
+    crystals.crystal1.value = Math.floor(Math.random() * 12 + 1);
+    crystals.crystal2.value = Math.floor(Math.random() * 12 + 1);
+    crystals.crystal3.value = Math.floor(Math.random() * 12 + 1);
+    crystals.crystal4.value = Math.floor(Math.random() * 12 + 1); 
+
     var counter = 0;
     $("#score-counter").append(counter).css('text-align', 'center');
+};
+//  FUNCTIONS
 
+// Counting crystal clicks
+$("#blue").on("click", function() {
+    counter = counter + crystals.crystal1.value;
+    console.log(counter);
+    $("#score-counter").text(counter);
+});
+
+$("#green").on("click", function() {
+    counter = counter + crystals.crystal2.value;
+    console.log(counter);
+    $("#score-counter").text(counter);
+});
+
+$("#yellow").on("click", function() {
+    counter = counter + crystals.crystal3.value;
+    console.log(counter);
+    $("#score-counter").text(counter);
+});
+
+$("#red").on("click", function() {
+    counter = counter + crystals.crystal4.value;
+    console.log(counter);
+    $("#score-counter").text(counter);
+});
+
+// Check if the player won or lost
+var checkScore = function() {
+    if (counter > targetRandomNum) {
+        alert("Sorry mate! You lose!");
+    }
 }
-// Functions
-
-
-
-
+   
 }) 
 
 
